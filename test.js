e16391be4986cbf8dde66cfc4876b2384a1f3a86
jonschlinkert <jon.schlinkert@sellside.com>
2015-03-18 05:52:20 -0400

    first commit

---
+++ test.js
@@ -0,0 +1,40 @@
+/*!
+ * map-cache <https://github.com/jonschlinkert/map-cache>
+ *
+ * Copyright (c) 2015, Jon Schlinkert.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+var assert = require('assert');
+var should = require('should');
+var MapCache = require('./');
+
+describe('mapCache', function () {
+  var cache = new MapCache;
+
+  it('should set values on the `__data__` object on the cache:', function () {
+    cache.set('a', 'b');
+    cache.should.eql({ __data__: { a: 'b' }});
+    cache.delete('a');
+  });
+
+  it('should delete values from the `__data__` object:', function () {
+    cache.set('a', 'b');
+    cache.delete('a');
+    cache.should.eql({ __data__: {}});
+  });
+
+  it('should get values from the `__data__` object:', function () {
+    cache.set('a', 'b');
+    cache.get('a').should.equal('b');
+    cache.get('a').should.not.equal('a');
+  });
+
+  it('should return `true` if `__data__` has `key`:', function () {
+    cache.set('a', 'b');
+    cache.has('a').should.be.true;
+    cache.has('a').should.not.be.false;
+  });
+});
